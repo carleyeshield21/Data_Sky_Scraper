26383358e07d5490d1ba95bdef8a7a0f5c8ec1b3
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-17 13:00:39 +0800

    commit

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ cursor.execute("SELECT * FROM roundtable WHERE knight = 'lancelot'")
 result = cursor.fetchall()
 print(result)
 
+# Inserting new rows
+neurow = [('perceival', 245), ('matahoti', 790)]
+cursor.executemany("INSERT INTO roundtable VALUES(?, ?)", neurow)
+connection.commit()
