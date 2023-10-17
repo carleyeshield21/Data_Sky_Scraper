26535b4204313f569fdab6dcff6a5393e8bef5fd
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-17 12:29:54 +0800

    commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import requests
 import smtplib
-import selectorlib
+import sqlite3
+
+connection = sqlite3.connect('sample')
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM roundtable WHERE knight = 'arthur'")
+result = cursor.fetchall()
+print(result)
