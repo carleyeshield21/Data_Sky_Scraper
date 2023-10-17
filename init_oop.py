fe302f3ff101b4d8abd70cab36673c293584bfb5
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-17 15:33:14 +0800

    commit

---
+++ init_oop.py
@@ -0,0 +1,15 @@
+class Poot:
+    def __init__(self, hext):
+        self.hext = hext
+        print('Taka')
+
+    def ano(self):
+        print('tisting lang')
+
+put = Poot(hext='inaka')
+print(put.hext)
+put.ano()
+
+put1 = Poot(hext='inamo')
+print(put1.hext)
+put1.ano()
