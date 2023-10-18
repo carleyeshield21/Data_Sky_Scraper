65f5204baef2d4518fa7886d84aa9175fc7dc833
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-18 13:06:34 +0800

    commit

--- code_testing.py
+++ code_testing.py
@@ -1,1 +1,9 @@
 import sqlite3
+
+connection = sqlite3.connect('Fam_Inc_Exp_database')
+cursor = connection.cursor()
+
+# Selecting all rows
+cursor.execute("SELECT * FROM 'Family Income and Expenditure'")
+result = cursor.fetchall()
+print(result)
