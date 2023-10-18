4881b3af4468abef172f68157a414f8d879075bf
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-18 13:11:56 +0800

    commit

--- code_testing.py
+++ code_testing.py
@@ -4,6 +4,13 @@ connection = sqlite3.connect('Fam_Inc_Exp_database')
 cursor = connection.cursor()
 
 # Selecting all rows
-cursor.execute("SELECT * FROM 'Family Income and Expenditure'")
+# cursor.execute("SELECT * FROM 'Family Income and Expenditure'")
+# result = cursor.fetchall()
+# print(result)
+
+# Selecting with specific values
+cursor.execute("SELECT * FROM 'Family Income and Expenditure' WHERE Region = 'CAR'")
 result = cursor.fetchall()
-print(result)
+for item in result:
+    print(item[1])
+    print('======')
